a4d804d3340dd4c5267af4770423f323958e6ac6
Gisele <rgisele644@gmail.com>
2023-06-20 20:25:16 -0300

    validação de formulário

--- validate.js
+++ validate.js
@@ -1,25 +1,82 @@
 const formulario = document.getElementById('form');
+const nomeInput = document.getElementById('nome');
+const emailInput = document.getElementById('email');
+const assuntoInput = document.getElementById('assunto');
 const span = document.querySelectorAll('.span-required');
 const campos = document.querySelectorAll('.required');
-const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-] +@ [a-zA-Z0 -9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const emailRegex = /^\w+[-+.']\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 
-function setError(index){
+
+
+nomeInput.addEventListener("click", (event) =>{
+   event.preventDefault();
+   nomeValidar(); 
+});
+
+emailInput.addEventListener("click", (event) =>{
+    event.preventDefault();
+    emailValidar();  
+ });
+
+ assuntoInput.addEventListener("click", (event) =>{
+    event.preventDefault();
+    assuntoValidar();
+ });
+ 
+ function validar(){
+    nomeValidar();
+    emailValidar();
+    assuntoValidar();
     
-    campos[index].style.border ='2px solid #ff1212';
-    span[index].style.display = 'block';
-}
-
-function removeError(index){
-    campos[index].style.border ='';
-    span[index].style.display = '';   
-}
+ }
 
 
-function nameValidate(){
-    if(campos[0].value.length < 3){
-        setError[0];
+ function nomeValidar(){
+
+    const nome = nomeInput.value
+
+    if(nome === ""){
+       error(0);
+
+        
     }
     else{
-        removeError[0];
+        removeError(0);
+
     }
-}
+ }
+
+ function emailValidar(){
+   if(emailInput.value.indexOf("@") == -1 || emailInput.value.indexOf(".") == -1 || (emailInput.value.indexOf(".")- emailInput.value.indexOf("@") == 1)){
+      error(1)
+   }else{
+    removeError(1)
+   }
+   
+ }
+
+ function assuntoValidar(){
+    const assunto = assuntoInput.value
+
+    if(assunto === ""){
+       error(2);
+
+        
+    }
+    else{
+        removeError(2);
+
+    }
+ }
+
+ 
+ function error(index){
+    span[index].style.display = 'block';
+    campos[index].style.border = '2px solid #ff1212';
+    
+ }
+ 
+ function removeError(index){
+    span[index].style.display = ''; 
+    campos[index].style.border = 'none';
+ }
